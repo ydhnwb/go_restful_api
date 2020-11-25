fe294e6d3e07cbc5a51e756f9a0022e1ee110f96
Prieyudha Akadita S <akaditasustono@gmail.com>
2020-11-25 22:42:05 +0700

    [WIP] trial error with gorm

--- entities/video.go
+++ entities/video.go
@@ -1,16 +1,23 @@
 package entities
 
+import "time"
+
 // Person struct is used for Video author
 type Person struct {
-	Firstname string `json:"firstname" binding:"required"`
-	Lastname  string `json:"lastname"`
-	Email     string `json:"email" binding:"required,email"`
+	ID        uint64 `gorm:"primary_key:auto_increment" json:"id"`
+	Firstname string `gorm:"type:varchar(100)" json:"firstname" binding:"required"`
+	Lastname  string `gorm:"type:varchar(100)" json:"lastname"`
+	Email     string `gorm:"type:varchar(255)" json:"email" binding:"required,email"`
 }
 
 // Video is a data class
 type Video struct {
-	Title       string `json:"title" binding:"min=1,max=100"`
-	Description string `json:"description" binding:"max=255"`
-	URL         string `json:"url" binding:"required,url"`
-	Author      Person `json:"author" binding:"required"`
+	ID          uint64    `gorm:"primary_key:auto_increment" json:"id"`
+	Title       string    `gorm:"type:varchar(255)" json:"title" binding:"min=1,max=255"`
+	Description string    `gorm:"type:text" json:"description" binding:"max=255"`
+	URL         string    `gorm:"type:text" json:"url" binding:"required,url"`
+	Author      Person    `gorm:"foreignkey:PersonID" json:"author" binding:"required"`
+	PersonID    uint64    `json:"-"`
+	CreatedAt   time.Time `json:"-" gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt   time.Time `json:"-" gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/mattn/go-sqlite3 v1.14.5 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/ugorji/go v1.2.0 // indirect
@@ -15,4 +16,6 @@ require (
 	golang.org/x/sys v0.0.0-20201116194326-cc9327a14d48 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
+	gorm.io/driver/sqlite v1.1.3 // indirect
+	gorm.io/gorm v1.20.7 // indirect
 )
