4b62d9f5058e8f68f313f42536d051ac8ca8f45c
Prieyudha Akadita S <akaditasustono@gmail.com>
2020-12-19 23:40:03 +0700

    Using pointer to avoid null when not using Preload

--- entities/user.go
+++ entities/user.go
@@ -2,10 +2,11 @@ package entities
 
 //User is represents users table in database
 type User struct {
-	ID       uint64 `gorm:"primary_key:auto_increment" json:"id"`
-	Fullname string `gorm:"type:varchar(255)" json:"fullname"`
-	Email    string `gorm:"uniqueIndex;type:varchar(255)" json:"email"`
-	Password string `gorm:"->;<-;not null" json:"-"`
-	Token    string `gorm:"-" binding:"-" json:"token,omitempty"`
-	Books    []Book `json:"books"`
+	ID       uint64  `gorm:"primary_key:auto_increment" json:"id"`
+	Photo    string  `gorm:"not null;default:"`
+	Fullname string  `gorm:"type:varchar(255)" json:"fullname"`
+	Email    string  `gorm:"uniqueIndex;type:varchar(255)" json:"email"`
+	Password string  `gorm:"->;<-;not null" json:"-"`
+	Token    string  `gorm:"-" binding:"-" json:"token,omitempty"`
+	Books    *[]Book `json:"books,omitempty"`
 }
